20c29c71756ad329560b2c0d314f869e5c51f13a
coskun cakir <csknckr@gmail.com>
2021-02-07 15:29:53 +0000

    avatar changed

--- src/components/avatar/index.js
+++ src/components/avatar/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import { graphql, StaticQuery } from "gatsby"
 import Img from "gatsby-image"
 
-export default function Avatar() {
+export function Avatar() {
   return (
     <StaticQuery
       query={query}
@@ -17,12 +17,35 @@ export default function Avatar() {
   )
 }
 
+export function AvatarHover() {
+  return (
+    <StaticQuery
+      query={query}
+      render={data => (
+        <Img
+          style={{ width: "inherit", height: "inherit" }}
+          fluid={data.avatarHover.sharp.fluid}
+          alt="Coskun Cakir"
+        />
+      )}
+    />
+  )
+}
+
 export const query = graphql`
   query {
     avatar: file(relativePath: { eq: "avatar.jpg" }) {
       sharp: childImageSharp {
         fluid {
-          ...GatsbyImageSharpFluid_withWebp_tracedSVG
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+
+    avatarHover: file(relativePath: { eq: "avatar_hover.jpg" }) {
+      sharp: childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_withWebp
         }
       }
     }
--- src/components/logo/index.js
+++ src/components/logo/index.js
@@ -1,20 +1,24 @@
 import React from "react"
 import { Link } from "gatsby"
 import { OvalBorder } from "../icons"
-import Avatar from "../avatar"
+import { Avatar, AvatarHover } from "../avatar"
 import cn from "classnames"
 import styles from "./logo.module.scss"
 
 export default function Logo({ className }) {
+  const [avatar, setAvatar] = React.useState(<Avatar />)
   return (
-    <Link to="/" className={cn(styles.logo, className)}>
+    <Link
+      to="/"
+      className={cn(styles.logo, className)}
+      onMouseEnter={() => setAvatar(<AvatarHover />)}
+      onMouseLeave={() => setAvatar(<Avatar />)}
+    >
       <div className={styles.avatar}>
         <div className={styles.oval}>
           <OvalBorder />
         </div>
-        <div className={styles.image}>
-          <Avatar />
-        </div>
+        <div className={styles.image}>{avatar}</div>
       </div>
       <div className={styles.title}>
         <strong>Coskun Cakir</strong>
