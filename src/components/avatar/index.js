7904c2ed291d122a392541e825a22820c4f68fd1
coskun cakir <csknckr@gmail.com>
2021-02-07 15:49:13 +0000

    avatar refactor

--- src/components/avatar/index.js
+++ src/components/avatar/index.js
@@ -2,29 +2,14 @@ import React from "react"
 import { graphql, StaticQuery } from "gatsby"
 import Img from "gatsby-image"
 
-export function Avatar() {
+export default function Avatar({ hover = false }) {
   return (
     <StaticQuery
       query={query}
       render={data => (
         <Img
           style={{ width: "inherit", height: "inherit" }}
-          fluid={data.avatar.sharp.fluid}
-          alt="Coskun Cakir"
-        />
-      )}
-    />
-  )
-}
-
-export function AvatarHover() {
-  return (
-    <StaticQuery
-      query={query}
-      render={data => (
-        <Img
-          style={{ width: "inherit", height: "inherit" }}
-          fluid={data.avatarHover.sharp.fluid}
+          fluid={hover ? data.avatarHover.sharp.fluid : data.avatar.sharp.fluid}
           alt="Coskun Cakir"
         />
       )}
--- src/components/logo/index.js
+++ src/components/logo/index.js
@@ -1,24 +1,26 @@
 import React from "react"
 import { Link } from "gatsby"
 import { OvalBorder } from "../icons"
-import { Avatar, AvatarHover } from "../avatar"
+import Avatar from "../avatar"
 import cn from "classnames"
 import styles from "./logo.module.scss"
 
 export default function Logo({ className }) {
-  const [avatar, setAvatar] = React.useState(<Avatar />)
+  const [avatarHover, setAvatarHover] = React.useState(false)
   return (
     <Link
       to="/"
       className={cn(styles.logo, className)}
-      onMouseEnter={() => setAvatar(<AvatarHover />)}
-      onMouseLeave={() => setAvatar(<Avatar />)}
+      onMouseEnter={() => setAvatarHover(true)}
+      onMouseLeave={() => setAvatarHover(false)}
     >
       <div className={styles.avatar}>
         <div className={styles.oval}>
           <OvalBorder />
         </div>
-        <div className={styles.image}>{avatar}</div>
+        <div className={styles.image}>
+          <Avatar hover={avatarHover} />
+        </div>
       </div>
       <div className={styles.title}>
         <strong>Coskun Cakir</strong>
